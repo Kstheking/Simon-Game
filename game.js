67a71122cff9a533a6ae144c2fd34e4b11a81c69
Khushal Sahni <kstheking0@gmail.com>
2019-03-26 17:29:13 +0530

    adding files

---
+++ game.js
@@ -0,0 +1,77 @@
+var buttonColours = ["red","blue","green","yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0, started = 0;
+
+function playSound(name){
+  var audio = new Audio("sounds/"+name+".mp3");
+  audio.play();
+}
+
+function animatePress(currentColour){
+  $('#'+currentColour).addClass("pressed");
+  setTimeout(function(){
+    $('#'+currentColour).removeClass("pressed");
+  },100);
+}
+
+function nextSequence(){
+  level++;
+  $("h1").text("Level "+level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $('#'+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+$(".btn").click(function(){
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(level);
+});
+
+$(document).keypress(function(event){
+  if(started === 0){
+    $("h1").text("Level "+level);
+    nextSequence();
+  }
+  started++;
+});
+
+function checkAnswer(currentLevel){
+  var correct = true;
+    for(var i = 0; i < userClickedPattern.length; i++){
+      if(userClickedPattern[i] != gamePattern[i]){
+        correct = false;
+        break;
+      }
+    }
+    if(correct){
+      if(userClickedPattern.length === gamePattern.length){
+        setTimeout(function(){
+          nextSequence();
+          userClickedPattern = [];
+        },1000);
+      }
+    }
+    else{
+      var audio = new Audio("sounds/wrong.mp3");
+      audio.play();
+      $("body").addClass("game-over");
+      setTimeout(function(){
+        $("body").removeClass("game-over");
+      },200);
+      $("h1").text("Game Over, Press Any Key to Restart");
+      startOver();
+    }
+}
+
+function startOver(){
+  started = 0;
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+}
